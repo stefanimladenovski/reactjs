ae96d1acec30b77e120aa3d9aa16362420da96d1
stefaninelkovska <stefaninelkovska@yahoo.com>
2022-09-07 15:00:04 +0200

    ADD cas4

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,12 +1,68 @@
+import {useEffect, useState} from "react"
+
 import { Input } from "./components/common/Input";
 
 const App = () => {
+  const [fieldType, setFieldType] = useState("password")
 
-  return (
-    <div>
-      <Input name="something" grid="blabla" lastname="BLAAAB0" />
-    </div>
-  );
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [comment, setComment] = useState("")
+
+  useEffect(() => {
+    console.log('comment was change to ', comment)
+  }, [comment])
+
+  const onMouseDownHandler = () => {
+    setFieldType("text")
+  }
+  const onMouseUpHandler = () => {
+    setFieldType("password")
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDeafult()
+    console.log({
+      username,
+      password,
+      comment
+    })
+  }
+
+  return (<>
+
+    <form onSubmit={handleSubmit}>
+
+      <Input
+        type="text"
+        name="username"
+        placeholder="Please enter Username"
+        value={username}
+        onChange={ (event) => {setUsername(event.target.value)}}
+      />
+
+      <Input
+        type={fieldType}
+        name="password"
+        placeholder="Please enter Password"
+        mouseDown={onMouseDownHandler}
+        mouseUp={onMouseUpHandler}
+        value={password}
+        onChange={ (event) => {setPassword(event.target.value)}}
+      />
+      
+      <Input
+        type="text"
+        name="comment"
+        placeholder="Please enter Comment"
+        value={comment}
+        onChange={ (event) => {setComment(event.target.value)}}
+      />
+      <br />
+
+      <button type="submit">SUBMIT</button>
+    </form>
+  </>);
 }
 
 export default App
--- reactjs/src/components/common/Input.js
+++ reactjs/src/components/common/Input.js
@@ -6,16 +6,41 @@ export const Input = ({
     name,
     id,
     mouseDown,
-    MouseUp,
+    mouseUp,
     ...restProps
 }) => {
-    return (
-        <input 
-        type= {type}
-        placeholder = {placeholder}
+    const iconType = type === "password" ? "fa fa-eye-slash" : "fa fa-eye"
+
+    const shoudDisplayTextArea = () => {
+        return name === "comment" && value && value.length > 8
+    }
+
+    return ( <>
+    { shoudDisplayTextArea() ?
+    <textarea 
+        name={name} 
+        id={id} 
+        cols="22" 
+        rows="3"
         value = {value}
         onChange = {onChange}
-        name = {name}
-        id = {id} />
-    )
+    /> :
+    <input 
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        name={name}
+        id={id}    
+    /> }
+
+    {name === "password" &&
+        <button
+            onMouseDown={mouseDown}
+            onMouseUp ={mouseUp}
+        >
+            <i className={iconType}></i>
+        </button>
+    }
+    </>)
 }
